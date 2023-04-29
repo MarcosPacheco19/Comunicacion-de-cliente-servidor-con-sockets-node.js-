eaad582378b80c0369185d393e156138dca4b1a0
MarcosPacheco19 <65028325+MarcosPacheco19@users.noreply.github.com>
2023-04-29 18:42:01 -0500

    Creacion en client

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const datosDir = path.resolve(__dirname,`../client` )
+
+const http = require('http')
+const server = http.createServer(app)
+
+app.get('/', (req, res)=> {
+    res.sendFile(`${datosDir}/index.html`)
+})
+
+server.listen(5050, ()=>{
+    console.log('El servidor esta en linea')
+    console.log('link: http://localhost:5050')
+})
