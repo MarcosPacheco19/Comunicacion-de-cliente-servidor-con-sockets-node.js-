38c8f0ec5e5f74b09408ea3773d3eaaf92ad717c
MarcosPacheco19 <65028325+MarcosPacheco19@users.noreply.github.com>
2023-04-29 21:11:14 -0500

    cambios en app.js

--- server/app.js
+++ server/app.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const path = require('path')
+const http = require('http')
+
+const server = http.createServer(app)
+const { server } = require('socket.io')
+const io = new Server(server)
 
 const app = express()
-
 const datosDir = path.resolve(__dirname,`../client` )
 
-const http = require('http')
-const server = http.createServer(app)
+io.on('connection', (socket)=>{
+    //console.log('Un usuario se ha conectado')
+
+    //socket.on('disconnect', ()=>{
+      //  console.log('Un usuario se ha desconectado')
+    //})
+    socket.on('chat', (msg)=>{
+        //console.log(msg)
+        io.emit('chat', msg)
+    })
+})
 
 app.get('/', (req, res)=> {
     res.sendFile(`${datosDir}/cliente.html`)
